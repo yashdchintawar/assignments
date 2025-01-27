7c7b461bcc5831a1a42d5a6b66a93f9dd14d3e16
Yash Chintawar <yashchintawar31@gmail.com>
2025-01-27 08:56:35 +0530

    push

---
+++ week-5/class.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+var createRes = function (status, data, message) {
+  return { status, data, message };
+};
+
+app.get("/add", function (req, res) {
+  const num1 = Number(req.query.a);
+  const num2 = Number(req.query.b);
+  const add = num1 + num2;
+  res.send(createRes("sucess", add, "addition operattion is completed"));
+});
+
+app.get("/multiply", function (req, res) {
+  const num1 = Number(req.query.a);
+  const num2 = Number(req.query.b);
+  const mul = num1 * num2;
+  res.send(createRes("sucess", mul, "Multiplication operattion is completed"));
+});
+
+app.get("/divide", function (req, res) {
+  const num1 = Number(req.query.a);
+  const num2 = Number(req.query.b);
+  const div = num1 / num2;
+  res.send(createRes("sucess", div, "Division operattion is completed"));
+});
+
+app.get("/subtract", function (req, res) {
+  const num1 = Number(req.query.a);
+  const num2 = Number(req.query.b);
+  const sub = num1 - num2;
+  res.send(createRes("sucess", add, "Substraction operattion is completed"));
+});
+
+app.listen("3331");
