a63bcb2900bfe064e09e7f612219a72ff06f47dc
Yash Chintawar <yashchintawar31@gmail.com>
2025-01-28 08:02:47 +0530

    class assignement for add sub mul div with routes

--- week-5/class.js
+++ week-5/class.js
@@ -6,31 +6,31 @@ var createRes = function (status, data, message) {
 };
 
 app.get("/add", function (req, res) {
-  const num1 = Number(req.query.a);
-  const num2 = Number(req.query.b);
+  const num1 = parseInt(req.query.a);
+  const num2 = parseInt(req.query.b);
   const add = num1 + num2;
   res.send(createRes("sucess", add, "addition operattion is completed"));
 });
 
 app.get("/multiply", function (req, res) {
-  const num1 = Number(req.query.a);
-  const num2 = Number(req.query.b);
+  const num1 = parseInt(req.query.a);
+  const num2 = parseInt(req.query.b);
   const mul = num1 * num2;
   res.send(createRes("sucess", mul, "Multiplication operattion is completed"));
 });
 
 app.get("/divide", function (req, res) {
-  const num1 = Number(req.query.a);
-  const num2 = Number(req.query.b);
+  const num1 = parseInt(req.query.a);
+  const num2 = parseInt(req.query.b);
   const div = num1 / num2;
   res.send(createRes("sucess", div, "Division operattion is completed"));
 });
 
 app.get("/subtract", function (req, res) {
-  const num1 = Number(req.query.a);
-  const num2 = Number(req.query.b);
+  const num1 = parseInt(req.query.a);
+  const num2 = parseInt(req.query.b);
   const sub = num1 - num2;
-  res.send(createRes("sucess", add, "Substraction operattion is completed"));
+  res.send(createRes("sucess", sub, "Substraction operattion is completed"));
 });
 
 app.listen("3331");
